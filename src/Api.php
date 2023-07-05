ce567d32736a06286e567fab8346c0c23633d73f
Pelgow <paul.libon@edu.devinci.fr>
2023-07-06 00:05:39 +0200

    scrap fonctionnel

--- src/Api.php
+++ src/Api.php
@@ -8,12 +8,14 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Api
 {
-    public static function scrapeCharacters()
+    public static function scrapeCharacters(): array
     {
         $url = 'https://www.giantbomb.com/inazuma-eleven/3025-1280/characters/';
         $crawler = new Crawler();
         $html = file_get_contents($url);
-        $crawler->addHtmlContent($html);
+        if ($html !== false) {
+            $crawler->addHtmlContent($html);
+        }
         $characterData = [];
         $crawler->filter('ul.editorial li')->each(function (Crawler $characterCrawler) use (&$characterData) {
             $name = $characterCrawler->filter('h3.title')->text();
