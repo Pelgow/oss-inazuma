d7cd6e79a93f62affc5910c9294fe7addfed7c52
Pelgow <paul.libon@edu.devinci.fr>
2023-07-05 23:21:42 +0200

    Plus ou moins ajoute - Test php

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -11,5 +11,7 @@ class ApiTest extends TestCase
         $this->assertIsInt($api->getRandomNumber());
         $this->assertGreaterThanOrEqual(0, $api->getRandomNumber());
         $this->assertLessThanOrEqual(100, $api->getRandomNumber());
+        echo "\n";
+        echo "Random number: " . $api->getRandomNumber() . "\n";
     }
 }
